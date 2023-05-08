7882803751e7ce25d51e247aea58fafbbea44162
gom20 <rhaldud89@gmail.com>
2023-05-08 20:24:21 +0900

    bug resolved

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ console.log('working in develop branch');
 console.log('hotfix resolved');
 
 console.log('working in feature/TEST01');
-console.log('test1')
+console.log('test1');
 console.log('working in feature!!!');
 
 console.log('feature2');
+
+console.log('hotfix added');

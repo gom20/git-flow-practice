4cb85ccd07ebe9ff7815325a41f01ba6f06959a0
gom20 <rhaldud89@gmail.com>
2023-08-20 10:25:49 +0900

    Squash commit

--- rebase-practice/main.js
+++ rebase-practice/main.js
@@ -3,3 +3,21 @@ console.log('start in main');
 console.log('commit1 in test1');
 
 console.log('commit2 in test1');
+
+console.log('commit1 in test2');
+
+console.log('test');
+
+console.log('test');
+
+console.log('test');
+
+console.log('test');
+
+console.log('test');
+
+console.log('test');
+
+console.log('commit2 in test2');
+
+console.log('commit3 in test2');
